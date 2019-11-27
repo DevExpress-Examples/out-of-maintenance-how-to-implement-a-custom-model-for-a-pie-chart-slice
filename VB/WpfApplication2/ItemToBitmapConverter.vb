feb1ca8a9a5b77abf7a87e3d3a9ec023bb15bf7f
DevExpressExampleBot <fakeEmail>
2019-11-27 17:52:11 +0300

    Source auto update [skip ci]

---
+++ VB/WpfApplication2/ItemToBitmapConverter.vb
@@ -0,0 +1,32 @@
+﻿Imports DevExpress.Utils.Svg
+Imports DevExpress.Xpf.Core.Native
+Imports System
+Imports System.Globalization
+Imports System.IO
+Imports System.Windows
+Imports System.Windows.Data
+Imports System.Windows.Markup
+
+Namespace WpfApplication2
+	Public Class ItemToBitmapConverter
+		Inherits MarkupExtension
+		Implements IValueConverter
+
+		Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+			Dim path As String = "..\..\Images\" & value.ToString() & ".svg"
+			Dim svgStream = File.OpenRead(path)
+			If svgStream Is Nothing Then
+				Return Nothing
+			End If
+			Dim svgImage = SvgLoader.LoadFromStream(svgStream)
+			Dim size = New Size(svgImage.Width * ScreenHelper.ScaleX, svgImage.Height * ScreenHelper.ScaleX)
+			Return WpfSvgRenderer.CreateImageSource(svgImage, size, Nothing, Nothing, True)
+		End Function
+		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+			Throw New NotImplementedException()
+		End Function
+		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+			Return Me
+		End Function
+	End Class
+End Namespace
---
+++ VB/WpfApplication2/PieChartSelectionBehavior.vb
@@ -0,0 +1,86 @@
+﻿Imports DevExpress.Mvvm.UI.Interactivity
+Imports DevExpress.Xpf.Charts
+Imports System
+Imports System.Windows
+Imports System.Windows.Input
+Imports System.Windows.Media.Animation
+
+Namespace WpfApplication2
+	Public Class PieChartSelectionBehavior
+		Inherits Behavior(Of ChartControl)
+
+		Public Shared ReadOnly ExpandAnimationProperty As DependencyProperty = DependencyProperty.Register("ExpandAnimation", GetType(DoubleAnimation), GetType(PieChartSelectionBehavior), New PropertyMetadata(Nothing))
+		Public Shared ReadOnly CollapseAnimationProperty As DependencyProperty = DependencyProperty.Register("CollapseAnimation", GetType(DoubleAnimation), GetType(PieChartSelectionBehavior), New PropertyMetadata(Nothing))
+
+		Private Const clickDelta As Integer = 200
+
+		Private mouseDownTime As Date
+		Private currentExplodedPoint As SeriesPoint
+
+		Private ReadOnly Property Chart() As ChartControl
+			Get
+				Return AssociatedObject
+			End Get
+		End Property
+
+		Public Property ExpandAnimation() As DoubleAnimation
+			Get
+				Return CType(GetValue(ExpandAnimationProperty), DoubleAnimation)
+			End Get
+			Set(ByVal value As DoubleAnimation)
+				SetValue(ExpandAnimationProperty, value)
+			End Set
+		End Property
+		Public Property CollapseAnimation() As DoubleAnimation
+			Get
+				Return CType(GetValue(CollapseAnimationProperty), DoubleAnimation)
+			End Get
+			Set(ByVal value As DoubleAnimation)
+				SetValue(CollapseAnimationProperty, value)
+			End Set
+		End Property
+
+		Private Sub Chart_MouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+			Me.mouseDownTime = Date.Now
+		End Sub
+		Private Sub Chart_MouseUp(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+			Dim hitInfo As ChartHitInfo = Chart.CalcHitInfo(e.GetPosition(Chart))
+			If hitInfo Is Nothing OrElse hitInfo.SeriesPoint Is Nothing OrElse Not IsClick(Date.Now) Then
+				Return
+			End If
+			Animate(hitInfo.SeriesPoint)
+		End Sub
+		Private Sub Animate(ByVal point As SeriesPoint)
+			Dim distance As Double = PieSeries.GetExplodedDistance(point)
+			Dim storyBoard As New Storyboard()
+			Dim animation = If(distance > 0, CollapseAnimation, ExpandAnimation)
+			storyBoard.Children.Add(animation)
+			Storyboard.SetTarget(animation, point)
+			Storyboard.SetTargetProperty(animation, New PropertyPath(PieSeries.ExplodedDistanceProperty))
+			If distance = 0 Then
+				Dim onComplete As EventHandler = Nothing
+				onComplete = Sub(s, e)
+					RemoveHandler storyBoard.Completed, onComplete
+					If currentExplodedPoint IsNot Nothing Then
+						Dim p As SeriesPoint = currentExplodedPoint
+						currentExplodedPoint = Nothing
+						Animate(p)
+					End If
+					currentExplodedPoint = point
+				End Sub
+				AddHandler storyBoard.Completed, onComplete
+			Else
+				currentExplodedPoint = Nothing
+			End If
+			storyBoard.Begin()
+		End Sub
+		Private Function IsClick(ByVal mouseUpTime As Date) As Boolean
+			Return (mouseUpTime.Subtract(Me.mouseDownTime)).TotalMilliseconds < clickDelta
+		End Function
+		Protected Overrides Sub OnAttached()
+			MyBase.OnAttached()
+			AddHandler Chart.MouseDown, AddressOf Chart_MouseDown
+			AddHandler Chart.MouseUp, AddressOf Chart_MouseUp
+		End Sub
+	End Class
+End Namespace
